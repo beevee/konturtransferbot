684d11df9f211bbeda52e411bc214372f7747682
Alexey Kirpichnikov <alexkir@skbkontur.ru>
2018-04-25 19:07:47 +0500

    Write log to stdout

--- cmd/konturtransferbot/main.go
+++ cmd/konturtransferbot/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"io/ioutil"
 	"os"
 	"os/signal"
@@ -21,20 +20,13 @@ func main() {
 		TelegramToken string `short:"t" long:"telegram-token" description:"@KonturTransferBot Telegram token" env:"KONTUR_TRANSFER_BOT_TOKEN"`
 		ScheduleYaml  string `short:"s" long:"schedule-yaml" default:"schedule.yml" description:"YAML file with schedule" env:"KONTUR_TRANSFER_SCHEDULE_YAML"`
 		Timezone      string `short:"z" long:"timezone" default:"Asia/Yekaterinburg" description:"Local timezone" env:"KONTUR_TRANSFER_BOT_TIMEZONE"`
-		Logfile       string `short:"l" long:"logfile" default:"konturtransferbot.log" description:"Log file name" env:"KONTUR_TRANSFER_BOT_LOGFILE"`
 	}
 
 	if _, err := flags.Parse(&opts); err != nil {
 		os.Exit(0)
 	}
 
-	logfile, err := os.OpenFile(opts.Logfile, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "failed to open logfile %s: %s", opts.Logfile, err)
-		os.Exit(1)
-	}
-	defer logfile.Close()
-	logger := log.NewLogfmtLogger(log.NewSyncWriter(logfile))
+	logger := log.NewLogfmtLogger(log.NewSyncWriter(os.Stdout))
 	logger = log.With(logger, "ts", log.DefaultTimestampUTC)
 
 	yamlSchedule, err := ioutil.ReadFile(opts.ScheduleYaml)
